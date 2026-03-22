0b5df2fb4bafa914a27b0c618b990f981cef3d47
bkd <contact@bkd.lol>
2026-03-22 13:47:29 -0500

    {}

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addCollection("articles", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("articles/*.html")
+    return collectionApi
+      .getFilteredByGlob("articles/*.html")
       .sort((a, b) => b.date - a.date);
   });
 
