943e46becd997ca649c7f8ad9c2a499b425697ac
bkd <contact@bkd.lol>
2026-03-22 13:01:58 -0500

    added articles

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addCollection("articles", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("articles/*.html")
+      .sort((a, b) => b.date - a.date);
+  });
+
+  return {
+    dir: {
+      input: ".",
+      output: "_site"
+    }
+  };
+};
